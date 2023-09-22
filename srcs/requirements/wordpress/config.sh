8a797983d06a07b02f2e5673b09c56b28d096da7
Abdelbari Messah <amessah@e3r4p12.1337.ma>
2023-09-22 12:07:15 +0100

    push

--- srcs/requirements/wordpress/config.sh
+++ srcs/requirements/wordpress/config.sh
@@ -3,6 +3,7 @@
 sleep 10
 
 sed -i -e 's/listen =.*/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
+mkdir -p  /var/www/html/wordpress
 cd /var/www/html/wordpress
 
 wp core download --allow-root --path=/var/www/html/wordpress
